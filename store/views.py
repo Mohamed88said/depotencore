27785f325d7bcbd16e217b170728f4af733bb7ce
Mohamed88said <mohamedsaiddiallo88@gmail.com>
2025-07-31 09:11:09 +0000

    Correction de l'import messages dans store/views.py

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import JsonResponse, HttpResponse, Http404, HttpResponseForbidden
 from django.core.paginator import Paginator
 from django.db.models import Q, Avg, Count, Sum

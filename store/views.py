1caea7fd3313df09ba42ac2ef0ab9ed9ed05d360
Mohamed88said <mohamedsaiddiallo88@gmail.com>
2025-07-31 15:16:56 +0000

    Correction des erreurs de décorateur et d'import

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.http import JsonResponse, HttpResponse, Http404, HttpResponseForbidden
 from django.core.paginator import Paginator
 from django.db.models import Q, Avg, Count, Sum
@@ -36,6 +37,7 @@ from admin_panel.models import Report
 
 # === Vues de géolocalisation ===
 @login_required
+
 User = get_user_model()
 
 def geocode(request):
